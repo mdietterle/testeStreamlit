dbf90a94a65426ff5907ec235b5346707df8b52d
Martim Dietterle <martim.dietterle@catolicasc.org.br>
2022-09-29 20:36:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://gitlab.com/mdietterle/datarepository/-/raw/main/batalhas2GuerraMundial.json')
 
 @st.cache
 def load_data(nrows):
